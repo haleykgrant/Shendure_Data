4ab6e6873857c364c749e488110fc1e6c9f8592d
Haley Grant <hgrant3@jhmi.edu>
2020-04-18 12:52:52 -0400

    add download script

---
+++ srr_download.sh
@@ -0,0 +1,208 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -o sge/
+#$ -e sge/
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR212/003/SRR2129993/SRR2129993_1.fastq.gz -o SRR2129993_GSM1833219_BH01_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR212/003/SRR2129993/SRR2129993_2.fastq.gz -o SRR2129993_GSM1833219_BH01_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR212/004/SRR2129994/SRR2129994_1.fastq.gz -o SRR2129994_GSM1833220_IA01_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR212/004/SRR2129994/SRR2129994_2.fastq.gz -o SRR2129994_GSM1833220_IA01_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR212/005/SRR2129995/SRR2129995_1.fastq.gz -o SRR2129995_GSM1833221_IA02_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR212/005/SRR2129995/SRR2129995_2.fastq.gz -o SRR2129995_GSM1833221_IA02_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR212/006/SRR2129996/SRR2129996_1.fastq.gz -o SRR2129996_GSM1833222_IA03_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR212/006/SRR2129996/SRR2129996_2.fastq.gz -o SRR2129996_GSM1833222_IA03_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR212/007/SRR2129997/SRR2129997_1.fastq.gz -o SRR2129997_GSM1833223_IA04_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR212/007/SRR2129997/SRR2129997_2.fastq.gz -o SRR2129997_GSM1833223_IA04_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR212/008/SRR2129998/SRR2129998_1.fastq.gz -o SRR2129998_GSM1833224_IA05_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR212/008/SRR2129998/SRR2129998_2.fastq.gz -o SRR2129998_GSM1833224_IA05_Homo_sapiens_OTHER_2.fastq.gz
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR212/009/SRR2129999/SRR2129999_1.fastq.gz -o SRR2129999_GSM1833225_IA06_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR212/009/SRR2129999/SRR2129999_2.fastq.gz -o SRR2129999_GSM1833225_IA06_Homo_sapiens_OTHER_2.fastq.gz
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/001/SRR2130001/SRR2130001_1.fastq.gz -o SRR2130001_GSM1833227_IA08_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/001/SRR2130001/SRR2130001_2.fastq.gz -o SRR2130001_GSM1833227_IA08_Homo_sapiens_OTHER_2.fastq.gz
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/002/SRR2130002/SRR2130002_1.fastq.gz -o SRR2130002_GSM1833228_IC02_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/002/SRR2130002/SRR2130002_2.fastq.gz -o SRR2130002_GSM1833228_IC02_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/003/SRR2130003/SRR2130003_1.fastq.gz -o SRR2130003_GSM1833229_IC03_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/003/SRR2130003/SRR2130003_2.fastq.gz -o SRR2130003_GSM1833229_IC03_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/004/SRR2130004/SRR2130004_1.fastq.gz -o SRR2130004_GSM1833230_IC04_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/004/SRR2130004/SRR2130004_2.fastq.gz -o SRR2130004_GSM1833230_IC04_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/005/SRR2130005/SRR2130005_1.fastq.gz -o SRR2130005_GSM1833231_IC05_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/005/SRR2130005/SRR2130005_2.fastq.gz -o SRR2130005_GSM1833231_IC05_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/006/SRR2130006/SRR2130006_1.fastq.gz -o SRR2130006_GSM1833232_IC06_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/006/SRR2130006/SRR2130006_2.fastq.gz -o SRR2130006_GSM1833232_IC06_Homo_sapiens_OTHER_2.fastq.gz
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/007/SRR2130007/SRR2130007_1.fastq.gz -o SRR2130007_GSM1833233_IC08_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/007/SRR2130007/SRR2130007_2.fastq.gz -o SRR2130007_GSM1833233_IC08_Homo_sapiens_OTHER_2.fastq.gz
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/008/SRR2130008/SRR2130008_1.fastq.gz -o SRR2130008_GSM1833234_IC09_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/008/SRR2130008/SRR2130008_2.fastq.gz -o SRR2130008_GSM1833234_IC09_Homo_sapiens_OTHER_2.fastq.gz
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/009/SRR2130009/SRR2130009_1.fastq.gz -o SRR2130009_GSM1833235_IC10_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/009/SRR2130009/SRR2130009_2.fastq.gz -o SRR2130009_GSM1833235_IC10_Homo_sapiens_OTHER_2.fastq.gz
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/000/SRR2130010/SRR2130010_1.fastq.gz -o SRR2130010_GSM1833236_IC11_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/000/SRR2130010/SRR2130010_2.fastq.gz -o SRR2130010_GSM1833236_IC11_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/001/SRR2130011/SRR2130011_1.fastq.gz -o SRR2130011_GSM1833237_IC12_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/001/SRR2130011/SRR2130011_2.fastq.gz -o SRR2130011_GSM1833237_IC12_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/002/SRR2130012/SRR2130012_1.fastq.gz -o SRR2130012_GSM1833238_IC13_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/002/SRR2130012/SRR2130012_2.fastq.gz -o SRR2130012_GSM1833238_IC13_Homo_sapiens_OTHER_2.fastq.gz
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/003/SRR2130013/SRR2130013_1.fastq.gz -o SRR2130013_GSM1833239_IC14_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/003/SRR2130013/SRR2130013_2.fastq.gz -o SRR2130013_GSM1833239_IC14_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/004/SRR2130014/SRR2130014_1.fastq.gz -o SRR2130014_GSM1833240_IC15_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/004/SRR2130014/SRR2130014_2.fastq.gz -o SRR2130014_GSM1833240_IC15_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/005/SRR2130015/SRR2130015_1.fastq.gz -o SRR2130015_GSM1833241_IC16_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/005/SRR2130015/SRR2130015_2.fastq.gz -o SRR2130015_GSM1833241_IC16_Homo_sapiens_OTHER_2.fastq.gz
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/006/SRR2130016/SRR2130016_1.fastq.gz -o SRR2130016_GSM1833242_IC17_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/006/SRR2130016/SRR2130016_2.fastq.gz -o SRR2130016_GSM1833242_IC17_Homo_sapiens_OTHER_2.fastq.gz
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/007/SRR2130017/SRR2130017_1.fastq.gz -o SRR2130017_GSM1833243_IC18_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/007/SRR2130017/SRR2130017_2.fastq.gz -o SRR2130017_GSM1833243_IC18_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/008/SRR2130018/SRR2130018_1.fastq.gz -o SRR2130018_GSM1833244_IC19_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/008/SRR2130018/SRR2130018_2.fastq.gz -o SRR2130018_GSM1833244_IC19_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/009/SRR2130019/SRR2130019_1.fastq.gz -o SRR2130019_GSM1833245_IC20_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/009/SRR2130019/SRR2130019_2.fastq.gz -o SRR2130019_GSM1833245_IC20_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/000/SRR2130020/SRR2130020_1.fastq.gz -o SRR2130020_GSM1833246_IC21_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/000/SRR2130020/SRR2130020_2.fastq.gz -o SRR2130020_GSM1833246_IC21_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/001/SRR2130021/SRR2130021_1.fastq.gz -o SRR2130021_GSM1833247_IC22_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/001/SRR2130021/SRR2130021_2.fastq.gz -o SRR2130021_GSM1833247_IC22_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/002/SRR2130022/SRR2130022_1.fastq.gz -o SRR2130022_GSM1833248_IC23_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/002/SRR2130022/SRR2130022_2.fastq.gz -o SRR2130022_GSM1833248_IC23_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/003/SRR2130023/SRR2130023_1.fastq.gz -o SRR2130023_GSM1833249_IC24_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/003/SRR2130023/SRR2130023_2.fastq.gz -o SRR2130023_GSM1833249_IC24_Homo_sapiens_OTHER_2.fastq.gz
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/004/SRR2130024/SRR2130024_1.fastq.gz -o SRR2130024_GSM1833250_IC25_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/004/SRR2130024/SRR2130024_2.fastq.gz -o SRR2130024_GSM1833250_IC25_Homo_sapiens_OTHER_2.fastq.gz
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/005/SRR2130025/SRR2130025_1.fastq.gz -o SRR2130025_GSM1833251_IC26_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/005/SRR2130025/SRR2130025_2.fastq.gz -o SRR2130025_GSM1833251_IC26_Homo_sapiens_OTHER_2.fastq.gz
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/006/SRR2130026/SRR2130026_1.fastq.gz -o SRR2130026_GSM1833252_IC27_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/006/SRR2130026/SRR2130026_2.fastq.gz -o SRR2130026_GSM1833252_IC27_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/007/SRR2130027/SRR2130027_1.fastq.gz -o SRR2130027_GSM1833253_IC28_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/007/SRR2130027/SRR2130027_2.fastq.gz -o SRR2130027_GSM1833253_IC28_Homo_sapiens_OTHER_2.fastq.gz
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/008/SRR2130028/SRR2130028_1.fastq.gz -o SRR2130028_GSM1833254_IC29_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/008/SRR2130028/SRR2130028_2.fastq.gz -o SRR2130028_GSM1833254_IC29_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/009/SRR2130029/SRR2130029_1.fastq.gz -o SRR2130029_GSM1833255_IC30_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/009/SRR2130029/SRR2130029_2.fastq.gz -o SRR2130029_GSM1833255_IC30_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/000/SRR2130030/SRR2130030_1.fastq.gz -o SRR2130030_GSM1833256_IC32_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/000/SRR2130030/SRR2130030_2.fastq.gz -o SRR2130030_GSM1833256_IC32_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/001/SRR2130031/SRR2130031_1.fastq.gz -o SRR2130031_GSM1833257_IC33_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/001/SRR2130031/SRR2130031_2.fastq.gz -o SRR2130031_GSM1833257_IC33_Homo_sapiens_OTHER_2.fastq.gz
+
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/002/SRR2130032/SRR2130032_1.fastq.gz -o SRR2130032_GSM1833258_IC34_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/002/SRR2130032/SRR2130032_2.fastq.gz -o SRR2130032_GSM1833258_IC34_Homo_sapiens_OTHER_2.fastq.gz
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/003/SRR2130033/SRR2130033_1.fastq.gz -o SRR2130033_GSM1833259_IC35_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/003/SRR2130033/SRR2130033_2.fastq.gz -o SRR2130033_GSM1833259_IC35_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/004/SRR2130034/SRR2130034_1.fastq.gz -o SRR2130034_GSM1833260_IC36_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/004/SRR2130034/SRR2130034_2.fastq.gz -o SRR2130034_GSM1833260_IC36_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/006/SRR2130036/SRR2130036_1.fastq.gz -o SRR2130036_GSM1833262_IC38_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/006/SRR2130036/SRR2130036_2.fastq.gz -o SRR2130036_GSM1833262_IC38_Homo_sapiens_OTHER_2.fastq.gz
+
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/007/SRR2130037/SRR2130037_1.fastq.gz -o SRR2130037_GSM1833263_IC39_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/007/SRR2130037/SRR2130037_2.fastq.gz -o SRR2130037_GSM1833263_IC39_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/008/SRR2130038/SRR2130038_1.fastq.gz -o SRR2130038_GSM1833264_IC40_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/008/SRR2130038/SRR2130038_2.fastq.gz -o SRR2130038_GSM1833264_IC40_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/009/SRR2130039/SRR2130039_1.fastq.gz -o SRR2130039_GSM1833265_IC41_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/009/SRR2130039/SRR2130039_2.fastq.gz -o SRR2130039_GSM1833265_IC41_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/000/SRR2130040/SRR2130040_1.fastq.gz -o SRR2130040_GSM1833266_IC42_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/000/SRR2130040/SRR2130040_2.fastq.gz -o SRR2130040_GSM1833266_IC42_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/001/SRR2130041/SRR2130041_1.fastq.gz -o SRR2130041_GSM1833267_IC43_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/001/SRR2130041/SRR2130041_2.fastq.gz -o SRR2130041_GSM1833267_IC43_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/002/SRR2130042/SRR2130042_1.fastq.gz -o SRR2130042_GSM1833268_IC44_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/002/SRR2130042/SRR2130042_2.fastq.gz -o SRR2130042_GSM1833268_IC44_Homo_sapiens_OTHER_2.fastq.gz
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/003/SRR2130043/SRR2130043_1.fastq.gz -o SRR2130043_GSM1833269_IC46_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/003/SRR2130043/SRR2130043_2.fastq.gz -o SRR2130043_GSM1833269_IC46_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/004/SRR2130044/SRR2130044_1.fastq.gz -o SRR2130044_GSM1833270_IC47_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/004/SRR2130044/SRR2130044_2.fastq.gz -o SRR2130044_GSM1833270_IC47_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/005/SRR2130045/SRR2130045_1.fastq.gz -o SRR2130045_GSM1833271_IC48_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/005/SRR2130045/SRR2130045_2.fastq.gz -o SRR2130045_GSM1833271_IC48_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/006/SRR2130046/SRR2130046_1.fastq.gz -o SRR2130046_GSM1833272_IC49_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/006/SRR2130046/SRR2130046_2.fastq.gz -o SRR2130046_GSM1833272_IC49_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/007/SRR2130047/SRR2130047_1.fastq.gz -o SRR2130047_GSM1833273_IC50_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/007/SRR2130047/SRR2130047_2.fastq.gz -o SRR2130047_GSM1833273_IC50_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/008/SRR2130048/SRR2130048_1.fastq.gz -o SRR2130048_GSM1833274_IC51_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/008/SRR2130048/SRR2130048_2.fastq.gz -o SRR2130048_GSM1833274_IC51_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/009/SRR2130049/SRR2130049_1.fastq.gz -o SRR2130049_GSM1833275_IC52_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/009/SRR2130049/SRR2130049_2.fastq.gz -o SRR2130049_GSM1833275_IC52_Homo_sapiens_OTHER_2.fastq.gz
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/000/SRR2130050/SRR2130050_1.fastq.gz -o SRR2130050_GSM1833276_IH01_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/000/SRR2130050/SRR2130050_2.fastq.gz -o SRR2130050_GSM1833276_IH01_Homo_sapiens_OTHER_2.fastq.gz
+
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/002/SRR2130052/SRR2130052_1.fastq.gz -o SRR2130052_GSM1833278_IH03_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/002/SRR2130052/SRR2130052_2.fastq.gz -o SRR2130052_GSM1833278_IH03_Homo_sapiens_OTHER_2.fastq.gz
+
+
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/001/SRR2130051/SRR2130051_1.fastq.gz -o SRR2130051_GSM1833277_IH02_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/001/SRR2130051/SRR2130051_2.fastq.gz -o SRR2130051_GSM1833277_IH02_Homo_sapiens_OTHER_2.fastq.gz
+
+
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/005/SRR2130035/SRR2130035_1.fastq.gz -o SRR2130035_GSM1833261_IC37_Homo_sapiens_OTHER_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/005/SRR2130035/SRR2130035_2.fastq.gz -o SRR2130035_GSM1833261_IC37_Homo_sapiens_OTHER_2.fastq.gz
