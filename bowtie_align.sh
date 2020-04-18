bd44dbac73e8702362f84b704d48cbb4a4a21510
Haley Grant <hgrant3@jhmi.edu>
2020-04-18 13:49:25 -0400

    update directory info

--- bowtie_align.sh
+++ bowtie_align.sh
@@ -11,6 +11,8 @@ d=/data/nfs1/home/hgrant3/cancer_seek/shendure
 samplefile=$d/sra_names.txt;
 all_lines=`cat $samplefile`
 
+mkdir sam_files
+
 for sample in $all_lines;
 do
 # create variable for paired end files
@@ -26,7 +28,7 @@ f1_unzipped=$d/${sample}_1.fastq;
 f2_unzipped=$d/${sample}_2.fastq;
 
 # run bowtie alignment
-../../anaconda3/bin/bowtie --chunkmbs 200 --maxins 800 -v 2 --threads 4 --sam ../../anaconda3/bin/hg19 -1 $f1_unzipped -2 $f2_unzipped > "${sample}_aligned.sam"
+../../anaconda3/bin/bowtie --chunkmbs 200 --maxins 800 -v 2 --threads 4 --sam ../../anaconda3/bin/hg19 -1 $f1_unzipped -2 $f2_unzipped > ./sam_files/"${sample}_aligned.sam"
 
 # remove unzipped files to save space
 rm $f1_unzipped
